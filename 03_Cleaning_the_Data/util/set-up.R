0bcc38c556168bac83a864c05fd10f978689a595
Candace Savonen <cansav09@gmail.com>
2022-09-12 20:45:21 +0000

    save this version of the exercise

--- 03_Cleaning_the_Data/util/set-up.R
+++ 03_Cleaning_the_Data/util/set-up.R
@@ -9,16 +9,5 @@ root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 candy_df <- read_csv(file.path(root_dir, "01_Forming_Questions", "data", "raw", "candy-data.csv"), 
                      show_col_types = FALSE)
 
-candy_df <- candy_df %>% 
-  dplyr::mutate_at(
-    dplyr::vars(chocolate, 
-                fruity, 
-                caramel, 
-                peanutyalmondy, 
-                nougat, 
-                crispedricewafer, 
-                hard, 
-                bar, 
-                pluribus), as.logical)
 
 write_csv(candy_df, file.path(root_dir, "01_Forming_Questions", "data", "candy-data.csv"))
